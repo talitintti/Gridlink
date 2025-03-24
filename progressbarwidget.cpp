6d1ac18bd801e106bd1def96a8d41e7fc960bb90
talitintti <kjshdf@protonmail.com>
2025-03-24 13:04:52 +0200

    progressbarwidget first version

---
+++ progressbarwidget.cpp
@@ -0,0 +1,84 @@
+#include "progressbarwidget.h"
+#include "helpers.h"
+
+ProgressBarWidget::ProgressBarWidget(QWidget *parent)
+    : QWidget(parent), total_length_ms_(0), current_time_ms_(0) {
+
+
+    // Initialize UI elements
+    progress_bar_ = new QProgressBar(this);
+    progress_bar_->setRange(0, 100);
+    progress_bar_->setValue(0);
+
+    current_time_lab_ = new QLabel(this);
+    total_time_lab_ = new QLabel(this);
+
+    layout_ = new QHBoxLayout(this);
+    layout_->addWidget(current_time_lab_);
+    layout_->addWidget(progress_bar_);
+    layout_->addWidget(total_time_lab_);
+    setLayout(layout_);
+
+    // Timer for auto-progressing the bar
+    timer_ = new QTimer(this);
+    connect(timer_, &QTimer::timeout, this, &ProgressBarWidget::UpdateProgress);
+}
+
+void ProgressBarWidget::SetLength(unsigned seconds) {
+    UpdateTotalTime(seconds);
+}
+
+void ProgressBarWidget::SetPosition(unsigned ms) {
+    if (ms > total_length_ms_) return;
+    UpdateCurrentTime(ms);
+}
+
+void ProgressBarWidget::UpdateProgress() {
+    if (current_time_ms_ < total_length_ms_) {
+        UpdateCurrentTime(current_time_ms_ + update_interval_ms_);
+    } else {
+        timer_->stop();
+    }
+}
+
+void ProgressBarWidget::Renew(unsigned length_seconds) {
+    HandleStop();
+    UpdateTotalTime(length_seconds);
+    HandlePlay();
+}
+
+void ProgressBarWidget::HandlePause() {
+    timer_->stop();
+}
+
+void ProgressBarWidget::HandlePlay() {
+    timer_->start(update_interval_ms_);
+}
+
+void ProgressBarWidget::HandleStop() {
+    timer_->stop();
+    UpdateCurrentTime(0);
+}
+
+void ProgressBarWidget::UpdateCurrentTime(unsigned current_time_ms) {
+    current_time_ms_ = current_time_ms;
+    current_time_lab_->setText(FormattedTime(current_time_ms));
+    progress_bar_->setValue(current_time_ms);
+}
+
+void ProgressBarWidget::UpdateTotalTime(unsigned seconds) {
+    auto formatted_time = FormattedTime(seconds);
+    total_length_ms_ = seconds * 1000;
+    total_time_lab_->setText(FormattedTime(seconds));
+    progress_bar_->setRange(0, total_length_ms_);
+}
+
+
+void ProgressBarWidget::SetUpdateInterval(unsigned update_interval_ms) {
+    if (update_interval_ms == 0) return;
+    update_interval_ms_ = update_interval_ms;
+}
+
+bool ProgressBarWidget::IsOn() {
+    return timer_->isActive();
+}
---
+++ progressbarwidget.h
@@ -0,0 +1,44 @@
+#ifndef PROGRESSBARWIDGET_H
+#define PROGRESSBARWIDGET_H
+
+#include <QWidget>
+#include <QLabel>
+#include <QProgressBar>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QTimer>
+
+class ProgressBarWidget : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit ProgressBarWidget(QWidget *parent = nullptr);
+
+    bool IsOn();
+    void SetLength(unsigned seconds);  // Set total song duration in seconds
+    void SetPosition(unsigned seconds);    // Update progress bar with current time
+    void HandlePlay();
+    void HandlePause();
+    void HandleStop();
+    void Renew(unsigned length_seconds); //HandleStop, set new length, HandlePlay
+
+    void SetUpdateInterval(unsigned interval_ms);
+
+private:
+    QProgressBar *progress_bar_;
+    QTimer *timer_;
+    QLabel *current_time_lab_;
+    QLabel *total_time_lab_;
+    QHBoxLayout *layout_;
+
+    unsigned total_length_ms_;  // Total duration of song in seconds
+    unsigned current_time_ms_; // Current position in seconds
+
+    unsigned update_interval_ms_;
+
+    void UpdateProgress();
+    void UpdateCurrentTime(unsigned ms);
+    void UpdateTotalTime(unsigned ms);
+};
+
+#endif // PROGRESSBARWIDGET_H
